de486dd0be66fa067373828dd554c63f5ae0eade
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-08-31 14:23:29 -0300

    resolve exercício aula09E6

--- T1/aula09/src/aula09/aula09E5.java
+++ T1/aula09/src/aula09/aula09E5.java
@@ -18,34 +18,37 @@ public class aula09E5 {
 		System.out.println("5 - Refrigerante - R$ 8.00");
 		System.out.println("6 - Suco de laranja - R$ 13.00");
 		int opcao = scanner.nextInt();		
-		System.out.println("Quantas unidades você deseja?");
-		int qtd = scanner.nextInt();		
 		
-		switch (opcao) {
-		case 1:
-			valorTotal = qtd * 10;
-			break;
-		case 2:
-			valorTotal = qtd * 15;
-			break;
-		case 3:
-			valorTotal = qtd * 18;
-			break;
-		case 4:
-			valorTotal = qtd * 12;
-			break;
-		case 5:
-			valorTotal = qtd * 8;
-			break;
-		case 6:
-			valorTotal = qtd * 13;
-			break;
-		default:
+		if (opcao < 1 || opcao > 6) {
 			System.out.println("Opção inválida");
-			break;
+		} else {
+			System.out.println("Quantas unidades você deseja?");
+			int qtd = scanner.nextInt();
+			
+			switch (opcao) {
+			case 1:
+				valorTotal = qtd * 10;
+				break;
+			case 2:
+				valorTotal = qtd * 15;
+				break;
+			case 3:
+				valorTotal = qtd * 18;
+				break;
+			case 4:
+				valorTotal = qtd * 12;
+				break;
+			case 5:
+				valorTotal = qtd * 8;
+				break;
+			case 6:
+				valorTotal = qtd * 13;
+				break;
+			}
+			System.out.println("O valor total é de R$ " + valorTotal);
 		}
 		
-		System.out.println("O valor total é de R$ " + valorTotal);
+		
 		
 		scanner.close();
 	}
