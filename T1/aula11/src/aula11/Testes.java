5990dd94c5f15ed145c5a561c3c439de72ba4c2b
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-09-04 16:30:15 -0300

    resolve exercício collectionsE1

---
+++ T1/aula11/src/aula11/Testes.java
@@ -0,0 +1,26 @@
+package aula11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Testes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		List<Integer> myArrIntegers = new ArrayList<Integer>();
+		
+		myArrIntegers.add(1);
+		myArrIntegers.add(3);
+		myArrIntegers.add(5);
+		myArrIntegers.add(6);
+		myArrIntegers.add(4);
+		myArrIntegers.add(2);
+		myArrIntegers.add(7);
+		
+		System.out.println(myArrIntegers.indexOf(1));
+
+		sc.close();
+	}
+}
