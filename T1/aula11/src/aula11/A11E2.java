5a36311a64449142ada2d67960ba3e8aed0b9a6d
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-09-04 14:19:10 -0300

    resolve exercício A11E4

---
+++ T1/aula11/src/aula11/A11E2.java
@@ -0,0 +1,13 @@
+package aula11;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class A11E2 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+	}
+}
