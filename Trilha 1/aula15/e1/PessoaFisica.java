1a40519bfdf9d63e21d347e472db4f6af1a20e52
Matheus Sousa <dsousamt@gmail.com>
2023-09-12 14:14:19 -0300

    Resolve exercícios da Aula 15

---
+++ Trilha 1/aula15/e1/PessoaFisica.java
@@ -0,0 +1,28 @@
+package aula15.e1;
+
+public class PessoaFisica extends Cliente{
+	private long cpf;
+	
+	public PessoaFisica() {
+		super();
+	}
+
+	public PessoaFisica(int id, String nome, int idade, String email, String telefone, long cpf) {
+		super(id, nome, idade, email, telefone);
+		this.cpf = cpf;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public void visualizarCliente() {
+		super.visualizarCliente();
+		System.out.println(this.cpf);
+	}
+}
